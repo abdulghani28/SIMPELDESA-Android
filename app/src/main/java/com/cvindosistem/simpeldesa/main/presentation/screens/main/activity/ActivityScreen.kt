84b3684e776eda0534b6109bb48898b038bdf1d5
Ghani (Kantor) <office.mhdabdulghani@gmail.com>
2025-05-27 13:04:11 +0700

    AktivitasPesananTab.kt

--- app/src/main/java/com/cvindosistem/simpeldesa/main/presentation/screens/main/activity/ActivityScreen.kt
+++ app/src/main/java/com/cvindosistem/simpeldesa/main/presentation/screens/main/activity/ActivityScreen.kt
@@ -1,12 +1,16 @@
 package com.cvindosistem.simpeldesa.main.presentation.screens.main.activity
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.cvindosistem.simpeldesa.core.components.AnimatedTabContent
 import com.cvindosistem.simpeldesa.core.components.AppTab
@@ -24,6 +28,7 @@ fun AktivitasScreen(
     Scaffold(
         topBar = {
             Column {
+                Spacer(modifier = Modifier.height(16.dp))
                 AppTab(
                     selectedTab = selectedTab,
                     tabs = tabs,
