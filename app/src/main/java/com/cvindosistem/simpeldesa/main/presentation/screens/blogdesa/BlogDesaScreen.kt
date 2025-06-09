9fc78efeff2d467999d86259db5afa9b58e5c4b6
Ghani (Kantor) <office.mhdabdulghani@gmail.com>
2025-06-09 17:39:46 +0700

    Optimized

--- app/src/main/java/com/cvindosistem/simpeldesa/main/presentation/screens/blogdesa/BlogDesaScreen.kt
+++ app/src/main/java/com/cvindosistem/simpeldesa/main/presentation/screens/blogdesa/BlogDesaScreen.kt
@@ -1,6 +1,5 @@
 package com.cvindosistem.simpeldesa.main.presentation.screens.blogdesa
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -10,7 +9,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
