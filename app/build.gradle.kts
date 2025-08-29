03da8f6fac6e4b53a28e01082f337c0a71e9011c
Muhammad Abdul Ghani <ghani28muhammad@gmail.com>
2025-08-29 14:10:03 +0700

    Commit versioning

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = "com.cvindosistem.simpeldesa"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
